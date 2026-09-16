7d5d28fa64b50e88809f4e0d99df12a054992789
rohiteeh <rohitrajan1436@gmail.com>
2026-09-16 19:07:14 +0530

    Add explicit root handler and bundle public files for Vercel

--- src/server.js
+++ src/server.js
@@ -35,6 +35,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Root route handler for serverless deployments
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // Request logging (development)
 if (process.env.NODE_ENV !== 'test') {
   app.use((req, res, next) => {
